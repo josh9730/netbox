df36024fd145ff87423edc9c41c5750c9c97a573
Josh <jdickman@cenic.org>
2023-12-09 14:30:05 -0800

    edits to jumper job

--- scripts/jumpers.py
+++ scripts/jumpers.py
@@ -1,52 +1,39 @@
-from dcim.choices import LinkStatusChoices, CableTypeChoices
-from dcim.models import Device, FrontPort, Interface, Site, Cable, DeviceRole, Rack
+from typing import Final, TypeAlias
+
+from dcim.choices import CableTypeChoices, LinkStatusChoices
+from dcim.models import Cable, Device, DeviceRole, FrontPort, Interface, RearPort, Site
 from extras.scripts import ChoiceVar, ObjectVar, Script, StringVar
 from utilities.exceptions import AbortScript
 
-from typing import Final
-
 name = "Jumper Creations"
+
+Ports: TypeAlias = FrontPort | Interface | RearPort
 
 
 class CableRunner:
     PANEL_ROLE: Final = DeviceRole.objects.get(slug="modular-panels")
-    rack_1_free_modular_ports: list[FrontPort] = None
-    rack_2_free_modular_ports: list[FrontPort] = None
-    connections: list[list[FrontPort | Interface]] = None
+    rack_1_free_modular_ports: list[FrontPort]
+    rack_2_free_modular_ports: list[FrontPort]
+    connections: list[list[Ports]]
 
     def __init__(
         self,
-        rack_1: Rack,
-        rack_1_port: FrontPort | Interface,
-        rack_2: Rack,
-        rack_2_port: FrontPort | Interface,
+        rack_1_port: Ports,
+        rack_2_port: Ports,
         clr: str,
         status: LinkStatusChoices = LinkStatusChoices.STATUS_CONNECTED,
         cable_type: CableTypeChoices = CableTypeChoices.TYPE_SMF_OS2,
     ) -> None:
-        self.rack_1 = rack_1
+        self.rack_1 = rack_1_port.device.rack
         self.rack_1_port = rack_1_port
-        self.rack_2 = rack_2
+        self.rack_2 = rack_2_port.device.rack
         self.rack_2_port = rack_2_port
-
-        self.data = {'clr': clr, 'status': status, 'cable_type': cable_type}
-        self._find_free_local_ports()
-
-    def _find_free_local_ports(self) -> None:
-        """Get all free modular panel ports for each rack."""
-        self.rack_1_free_modular_ports = list(
-            FrontPort.objects.filter(device__role=self.PANEL_ROLE, device__rack=self.rack_1, cable=None)
-        )
-        self.rack_2_free_modular_ports = list(
-            FrontPort.objects.filter(device__role=self.PANEL_ROLE, device__rack=self.rack_2, cable=None)
-        )
-        if not all((self.rack_1_free_modular_ports, self.rack_1_free_modular_ports)):
-            raise AbortScript("No free modular panel ports found between the selected racks.")
+        self.data = {"clr": clr, "status": status, "cable_type": cable_type}
 
     @staticmethod
     def create_cable_single(
-        port_1: FrontPort | Interface,
-        port_2: FrontPort | Interface,
+        port_1: Ports,
+        port_2: Ports,
         clr: str,
         status: LinkStatusChoices = LinkStatusChoices.STATUS_CONNECTED,
         cable_type: CableTypeChoices = CableTypeChoices.TYPE_SMF_OS2,
@@ -62,7 +49,19 @@ class CableRunner:
         cable.save()
         return cable
 
-    def _validate_free_ports(self, ports: list[FrontPort]) -> list[FrontPort]:
+    def _find_free_local_ports(self) -> None:
+        """Get all free modular panel ports for each rack."""
+        self.rack_1_free_modular_ports = list(
+            FrontPort.objects.filter(device__role=self.PANEL_ROLE, device__rack=self.rack_1, cable=None)
+        )
+        self.rack_2_free_modular_ports = list(
+            FrontPort.objects.filter(device__role=self.PANEL_ROLE, device__rack=self.rack_2, cable=None)
+        )
+        if not all((self.rack_1_free_modular_ports, self.rack_1_free_modular_ports)):
+            raise AbortScript("No free modular panel ports found between the selected racks.")
+
+    @staticmethod
+    def _validate_free_ports(ports: list[FrontPort]) -> list[FrontPort]:
         """Validate all locally free ports are also free on the remote side.
 
         - Find the remote_panel, if ValueError, then the two panels are not cabled
@@ -71,7 +70,7 @@ class CableRunner:
         Note the returned list is in the form of list[list[LOCALPORT, REMOTEPORT]], where local indicates the
         rack that was checked.
         """
-        valid_remote_ports = []
+        valid_ports = []
         for port in ports:
             try:
                 remote_panel = port.rear_port.link_peers[0].device
@@ -80,11 +79,11 @@ class CableRunner:
 
             remote_port = FrontPort.objects.get(device=remote_panel, rear_port_position=port.rear_port_position)
             if not remote_port.cable:
-                valid_remote_ports.append([port, remote_port])
+                valid_ports.append([port, remote_port])
 
-        if not valid_remote_ports:
-            raise AbortScript('No valid modular ports found.')
-        return valid_remote_ports
+        if not valid_ports:
+            raise AbortScript("No valid modular ports found.")
+        return valid_ports
 
     def get_connections(self) -> None:
         """Get valid connections, where the connections are a list of endpoints that will be passed directly
@@ -104,21 +103,26 @@ class CableRunner:
         SPK-HUB-SPK will be a length of three.
         """
 
-        def filter_ports_direct_connect(ports: list[list[FrontPort]]) -> None | list[FrontPort]:
+        def filter_ports_direct_connect(
+            ports: list[list[FrontPort]],
+        ) -> None | list[FrontPort]:
             """Check if remote ports are on rack_2, i.e. Rack1 and Rack2 are directly connected."""
             direct_connect_ports = [p for p in ports if p[1].device.rack == self.rack_2]
             if direct_connect_ports:
                 return direct_connect_ports[0]
             else:
-                None
+                return None
 
+        self._find_free_local_ports()
         rack_1_panel_ports = self._validate_free_ports(self.rack_1_free_modular_ports)
-
         direct_ports = filter_ports_direct_connect(rack_1_panel_ports)
 
         # SPK-SPK or SPK-HUB
         if direct_ports:
-            self.connections = [[self.rack_1_port, direct_ports[0]], [self.rack_2_port, direct_ports[1]]]
+            self.connections = [
+                [self.rack_1_port, direct_ports[0]],
+                [self.rack_2_port, direct_ports[1]],
+            ]
 
         # SPK-HUB-SPK
         else:
@@ -133,12 +137,13 @@ class CableRunner:
     def create_cables(self) -> list[Cable]:
         cables = []
         for connection in self.connections:
-            self.data.update({'port_1': connection[0], 'port_2': connection[1]})
+            self.data.update({"port_1": connection[0], "port_2": connection[1]})
             cables.append(self.create_cable_single(**self.data))
         return cables
 
 
 class FormFields:
+    port_description = "One of {}-Side Interface, FrontPort, or RearPort must be selected."
     site = ObjectVar(
         model=Site,
     )
@@ -150,16 +155,23 @@ class FormFields:
     a_interface = ObjectVar(
         model=Interface,
         label="A-Interface",
-        description="One of A-Interface or A-FrontPort must be selected.",
+        description=port_description.format("A"),
         required=False,
-        query_params={"device_id": "$a_device", 'cabled': False},
+        query_params={"device_id": "$a_device", "cabled": False},
     )
     a_frontport = ObjectVar(
         model=FrontPort,
-        label="A-FrontPort/Optical",
-        description="One of A-Interface or A-FrontPort must be selected.",
+        label="A-FrontPort/Optical Client",
+        description=port_description.format("A"),
         required=False,
-        query_params={"device_id": "$a_device", 'cabled': False},
+        query_params={"device_id": "$a_device", "cabled": False},
+    )
+    a_rearport = ObjectVar(
+        model=RearPort,
+        label="A-RearPort/Optical Trunk",
+        description=port_description.format("A"),
+        required=False,
+        query_params={"device_id": "$a_device", "cabled": False},
     )
 
     z_device = ObjectVar(
@@ -170,23 +182,34 @@ class FormFields:
     z_interface = ObjectVar(
         model=Interface,
         label="Z-Interface",
-        description="One of Z-Interface or Z-FrontPort must be selected.",
+        description=port_description.format("Z"),
         required=False,
-        query_params={"device_id": "$z_device", 'cabled': False},
+        query_params={"device_id": "$z_device", "cabled": False},
     )
     z_frontport = ObjectVar(
         model=FrontPort,
         label="Z-FrontPort|Optical",
-        description="One of Z-Interface or Z-FrontPort must be selected.",
+        description=port_description.format("Z"),
         required=False,
-        query_params={"device_id": "$z_device", 'cabled': False},
+        query_params={"device_id": "$z_device", "cabled": False},
     )
+    z_rearport = ObjectVar(
+        model=RearPort,
+        label="Z-RearPort/Optical Trunk",
+        description=port_description.format("A"),
+        required=False,
+        query_params={"device_id": "$z_device", "cabled": False},
+    )
+
     status = ChoiceVar(
         label="Cable Status",
         choices=LinkStatusChoices,
         default=LinkStatusChoices.STATUS_CONNECTED,
     )
-    clr = StringVar(label="CLR", description="Value should match `CLR-XXXX` or `nonprod STRING`.", default="CLR-1234")
+    clr = StringVar(
+        label="CLR",
+        description="Value should match `CLR-XXXX` or `nonprod STRING`.",
+    )
     cable_type = ChoiceVar(
         label="Cable Type",
         choices=CableTypeChoices,
@@ -194,35 +217,7 @@ class FormFields:
     )
 
 
-class JumperMixins:
-    @staticmethod
-    def wrap_save(obj):
-        obj.full_clean()
-        obj.save()
-
-    @staticmethod
-    def check_port_selections(data: dict) -> None:
-        """Only one of Interface/FrontPort should be selected for A and Z Side."""
-
-        def check_pairs(port_1: ObjectVar, port_2: ObjectVar, side: str) -> None:
-            msg = "Select **one** of `{} FrontPort / Interface`."
-            if all((port_1, port_2)):
-                raise AbortScript(msg.format(side))
-            if not any((port_1, port_2)):
-                raise AbortScript(msg.format(side))
-
-        check_pairs(data['a_interface'], data['a_frontport'], "A")
-        check_pairs(data['z_interface'], data['z_frontport'], "Z")
-
-    @staticmethod
-    def get_valid_ports(data: dict) -> tuple[[FrontPort | Interface], [FrontPort | Interface]]:
-        def find_port(side: str) -> FrontPort | Interface:
-            return data[f"{side}_interface"] if data[f"{side}_interface"] else data[f"{side}_frontport"]
-
-        return find_port('a'), find_port('z')
-
-
-class NewJumper(Script, JumperMixins):
+class NewJumper(Script):
     class Meta:
         name = "Create Jumpers"
         description = (
@@ -234,14 +229,30 @@ class NewJumper(Script, JumperMixins):
     a_device = FormFields.a_device
     a_interface = FormFields.a_interface
     a_frontport = FormFields.a_frontport
+    a_rearport = FormFields.a_rearport
     z_device = FormFields.z_device
     z_interface = FormFields.z_interface
     z_frontport = FormFields.z_frontport
+    z_rearport = FormFields.z_rearport
     status = FormFields.status
     clr = FormFields.clr
     cable_type = FormFields.cable_type
 
     def run(self, data, commit):
+        def check_port_selections(data: dict) -> tuple[Ports, Ports]:
+            """Only one of Interface/FrontPort should be selected for A and Z Side."""
+
+            def check_pairs(port_1: ObjectVar, port_2: ObjectVar, port_3: ObjectVar, side: str) -> Ports:
+                msg = "Select **one** of `{} FrontPort / Interface | RearPort`."
+                ports_input = [i for i in [port_1, port_2, port_3] if i]
+                if len(ports_input) > 1:
+                    raise AbortScript(msg.format(side))
+                return ports_input[0]
+
+            a_port = check_pairs(data["a_interface"], data["a_frontport"], data["a_rearport"], "A")
+            z_port = check_pairs(data["z_interface"], data["z_frontport"], data["a_rearport"], "Z")
+            return a_port, z_port
+
         def create_cable_log(data, cable: Cable) -> None:
             # fmt: off
             self.log_success(
@@ -258,30 +269,27 @@ class NewJumper(Script, JumperMixins):
             )
             # fmt: on
 
-        self.check_port_selections(data)
-        rack_1_port, rack_2_port = self.get_valid_ports(data)
+        rack_1_port, rack_2_port = check_port_selections(data)
 
         # Intra-rack connection
-        if data['a_device'].rack == data['z_device'].rack:
+        if data["a_device"].rack == data["z_device"].rack:
             cable = CableRunner.create_cable_single(
                 rack_1_port,
                 rack_2_port,
-                data['clr'],
-                data['status'],
-                data['cable_type'],
+                data["clr"],
+                data["status"],
+                data["cable_type"],
             )
             create_cable_log(data, cable)
 
         # Inter-rack connection
         else:
             runner = CableRunner(
-                data['a_device'].rack,
                 rack_1_port,
-                data['z_device'].rack,
                 rack_2_port,
-                data['clr'],
-                data['status'],
-                data['cable_type'],
+                data["clr"],
+                data["status"],
+                data["cable_type"],
             )
             runner.get_connections()
             cables = runner.create_cables()
